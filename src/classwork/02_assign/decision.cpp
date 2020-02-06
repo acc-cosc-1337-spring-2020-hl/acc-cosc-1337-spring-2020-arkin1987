6a0066a42fde741922c41faa80d6a2a48edcf6f5
David Arkin <david.arkin@g.austincc.edu>
2020-02-05 19:13:00 -0600

    Gross pay calculation

--- src/classwork/02_assign/decision.cpp
+++ src/classwork/02_assign/decision.cpp
@@ -1,1 +1,21 @@
+#include "decision.h"
 //Write the function code that returns the product of hours and hourly_rate.
+
+
+
+int gross_pay(int hours, int hourly_rate)
+{
+	if (hours >= 40)
+	{
+		return hourly_rate * 1.5 * (hours - 40) + (40 * hourly_rate);
+	
+	}
+	else
+	{
+		return hourly_rate * hours;
+
+	}
+	
+	
+	return hours * hourly_rate;
+}
--- src/classwork/02_assign/main.cpp
+++ src/classwork/02_assign/main.cpp
@@ -1,24 +1,31 @@
 //Write the include statement for decisions.h here
-
+#include "decisions.h"
 //Write include for capturing input from keyboard and displaying output to screen
-
+#include<iostream>
 
 //Write namespace using statements for cout and cin
-
+using std::cout; 
+using std::cin;
 int main() 
 {
 	//create a double variable named hours
 	//create a double variable named hourly_rate
-
+	int hours;
+	int hourly_rate;
 	//Prompt user for keyboard input for hours and hourly_rate
 	//save the input to variables hours and hourly_rate
-
+	cout << "Enter hours: ";
+	cin >> hours;
+	cout << "Enter hourly rate: ";
+	cin >> hourly_rate;
 	//Create a double variable named gross_salary
 	//Call the gross_pay function and save its return value to the gross_salary variable
+	int gross_salary;
 
+	
 	//Display the gross_salary variable as follows:
 	//Example ---->>>     Gross pay: 150.00   
-
+	cout << "Gross pay: " << gross_salary;
 	return 0;
 }
 
