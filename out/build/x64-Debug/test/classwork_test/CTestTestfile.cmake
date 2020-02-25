3ec0847293728829e9570468f1ba1f729376b3a6
David Arkin <david.arkin@g.austincc.edu>
2020-02-24 19:07:02 -0600

    remove

--- out/build/x64-Debug/test/classwork_test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/9419322/source/repos/acc-cosc-1337-spring-2020-arkin19875/test/classwork_test
-# Build directory: C:/Users/9419322/source/repos/acc-cosc-1337-spring-2020-arkin19875/out/build/x64-Debug/test/classwork_test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- out/build/x64-Debug/test/homework_test/03_iteration_test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/9419322/source/repos/acc-cosc-1337-spring-2020-arkin19875/test/homework_test/03_iteration_test
-# Build directory: C:/Users/9419322/source/repos/acc-cosc-1337-spring-2020-arkin19875/out/build/x64-Debug/test/homework_test/03_iteration_test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(hw_03_tests "C:/Users/9419322/source/repos/acc-cosc-1337-spring-2020-arkin19875/out/build/x64-Debug/test/homework_test/03_iteration_test/hw_03_tests.exe" "-s")
