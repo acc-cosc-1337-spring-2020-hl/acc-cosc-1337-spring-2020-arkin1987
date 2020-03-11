25cb1a9d2bc7b6fac733f9662a13105d42e96bb1
David Arkin <david.arkin@g.austincc.edu>
2020-03-11 14:06:19 -0500

    switch statement tests

--- out/build/x64-Debug/test/examples_test/cmake_install.cmake
+++ out/build/x64-Debug/test/examples_test/cmake_install.cmake
@@ -34,6 +34,7 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
+  include("C:/Users/Arkin/source/repos/acc-cosc-1337-spring-2020-arkin1987/out/build/x64-Debug/test/examples_test/02_module_test/cmake_install.cmake")
   include("C:/Users/Arkin/source/repos/acc-cosc-1337-spring-2020-arkin1987/out/build/x64-Debug/test/examples_test/04_module_test/cmake_install.cmake")
 
 endif()
